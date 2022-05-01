4aa004d50706cfb2805990f39dc2feb73e1a9c74
James Youngman <youngman@google.com>
2022-05-01 09:14:07 +0100

    [cpu] better doc comments for alarms.

--- cpu/src/alarm.rs
+++ cpu/src/alarm.rs
@@ -19,36 +19,75 @@ impl Display for BadMemOp {
     }
 }
 
-// Alarms from User's Handbook section 5-2.2
+// Alarms from User's Handbook section 5-2.2; full names are taken
+// from section 10-2.5.1 (vol 2) of the Technical Manual.
 #[derive(Debug)]
 pub enum Alarm {
-    PSAL(u32, String),                   // Program counter set to illegal address
-    OCSAL(Instruction, String),          // Illegal instruction was read into N register
-    QSAL(Instruction, BadMemOp, String), // Q register (i.e. data fetch address) is set to illegal address
-    ROUNDTUITAL(String),                 // Something is not implemented
-    // I/O Alarm in IOS instruction; device broken/maintenance/nonexistent.
+    /// P Memory Cycle Selecttion Alarm.  This fires when we attempt
+    /// to fetch an instruction from an invalid address.
+    PSAL(u32, String),
+
+    /// Operation Code Alarm.  This fires when an instruction word
+    /// containing an undefined operation code is read out of memory.
+    /// Section 10-2.5.3 states that this can also happen when an AOP
+    /// instruction specifies an undefined op code in bits N₂.₆-N₂.₁.
+    /// But I don't know what an AOP instruction is, yet.
+    OCSAL(Instruction, String),
+
+    /// Q Memory Cycle Selecttion Alarm.  Q register (i.e. data fetch
+    /// address) is set to an invalid address.
+    QSAL(Instruction, BadMemOp, String),
+
+    /// In-Out Selection Alarm. I/O Alarm in IOS instruction; device
+    /// broken/maintenance/nonexistent.
     IOSAL {
         unit: Unsigned6Bit,
         operand: Option<Unsigned18Bit>,
         message: String,
     },
 
-    // Missed-data alarm.
-    MISAL {
-        unit: Unsigned6Bit,
-    }, // Program too slow for I/O device.
+    /// In-Out Miss Indication Alarm.  Fires when some I/O unit has
+    /// missed a data item.  This generally indicates that the program
+    /// is too slow for an I/O device.  For example because it uses
+    /// too many hold bits.
+    MISAL { unit: Unsigned6Bit },
 
-    // There is a bug in the simulator.
-    BUGAL {
-        instr: Option<Instruction>,
-        message: String,
-    },
+    // Alarms we probably should implement but have not:
+    //
+    // SYAL: Sync System Alarm
 
-    // Loop in deferred addressing (detection of this is not a feature
-    // of the TX-2).
+    // Not included here because the simulator pretends that parity
+    // checks always succeed:
+    //
+    // MPAL: M Parity Alarm
+    // NPAL: N Parity Alarm
+    // FPAL: F Parity Alarm
+    // XPAL: X Parity Alarm
+    //
+    // Not included here for other reasons:
+    //
+    // TSAL: T Memory Selection Alarm; indicates overcurrent in the T
+    // Memory.  We have no hardware, so no overcurrent.
+    //
+    // Mousetrap Alarm: stops the computer when there is a malfunction
+    // in the setting of the S-memory flip-flops.  We don't have those
+    // in the simulator.
+
+    // The following alarms didn't exist in the real TX-2:
+    /// Something is not implemented
+    ROUNDTUITAL(String),
+
+    /// Loop in deferred addressing (detection of this is not a feature
+    /// of the TX-2).
     DEFERLOOPAL {
         /// address is some address within the loop.
         address: Unsigned18Bit,
+    },
+
+    /// There is a bug in the simulator.
+    BUGAL {
+        instr: Option<Instruction>,
+        message: String,
     },
 }
 
