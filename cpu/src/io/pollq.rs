cc88d6c60b97871f7bd6c56242a2663b3981f703
James Youngman <youngman@google.com>
2022-05-17 23:30:02 +0100

    Factor KeyedReversePriorityQueue out into the base crate.
    The motivation for this is that we might use this to schedule
    callbacks in the `cli` crate.

--- cpu/src/io/pollq.rs
+++ cpu/src/io/pollq.rs
@@ -1,57 +1,12 @@
-use std::cmp::Ordering;
+use std::fmt::Debug;
 use std::time::Duration;
 
-use keyed_priority_queue::KeyedPriorityQueue;
-
+use base::collections::pq::KeyedReversePriorityQueue;
 use base::prelude::*;
 
 #[derive(Debug)]
-struct ReverseOrdered<T> {
-    inner: T,
-}
-
-impl<T> From<T> for ReverseOrdered<T> {
-    fn from(inner: T) -> ReverseOrdered<T> {
-        ReverseOrdered { inner }
-    }
-}
-
-impl<T: Ord> PartialOrd for ReverseOrdered<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<T: Eq> Eq for ReverseOrdered<T> {}
-
-impl<T: Eq> PartialEq for ReverseOrdered<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.inner == other.inner
-    }
-}
-
-impl<T: Ord> Ord for ReverseOrdered<T> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self.inner.cmp(&other.inner) {
-            Ordering::Less => Ordering::Greater,
-            Ordering::Equal => Ordering::Equal,
-            Ordering::Greater => Ordering::Less,
-        }
-    }
-}
-
-#[test]
-fn test_reverse_order() {
-    assert_eq!(ReverseOrdered::from(1), ReverseOrdered::from(1));
-    assert_ne!(ReverseOrdered::from(1), ReverseOrdered::from(0));
-    assert!(ReverseOrdered::from(1) < ReverseOrdered::from(0));
-    assert_ne!(ReverseOrdered::from(1), ReverseOrdered::from(0));
-    assert!(!(ReverseOrdered::from(1) > ReverseOrdered::from(0)));
-}
-
-#[derive(Debug)]
 pub struct PollQueue {
-    items: KeyedPriorityQueue<SequenceNumber, ReverseOrdered<Duration>>,
+    items: KeyedReversePriorityQueue<SequenceNumber, Duration>,
 }
 
 #[derive(Debug)]
@@ -62,22 +17,20 @@ pub enum PollQueueUpdateFailure {
 impl PollQueue {
     pub fn new() -> PollQueue {
         PollQueue {
-            items: KeyedPriorityQueue::new(),
+            items: KeyedReversePriorityQueue::new(),
         }
     }
 
     pub fn peek(&self) -> Option<(&SequenceNumber, &Duration)> {
-        self.items.peek().map(|(k, p)| (k, &p.inner))
+        self.items.peek()
     }
 
     pub fn pop(&mut self) -> Option<(SequenceNumber, Duration)> {
-        self.items.pop().map(|(k, p)| (k, p.inner))
+        self.items.pop()
     }
 
     pub fn push(&mut self, key: SequenceNumber, priority: Duration) -> Option<Duration> {
-        self.items
-            .push(key, ReverseOrdered::from(priority))
-            .map(|rd| rd.inner)
+        self.items.push(key, priority)
     }
 
     pub fn update(
@@ -85,11 +38,8 @@ impl PollQueue {
         key: SequenceNumber,
         priority: Duration,
     ) -> Result<Duration, PollQueueUpdateFailure> {
-        match self
-            .items
-            .set_priority(&key, ReverseOrdered::from(priority))
-        {
-            Ok(priority) => Ok(priority.inner),
+        match self.items.set_priority(&key, priority) {
+            Ok(priority) => Ok(priority),
             Err(_) => Err(PollQueueUpdateFailure::UnknownSequence(key)),
         }
     }
