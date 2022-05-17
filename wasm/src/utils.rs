3f445ed63cd34538fb47cbe871129b1774b6cb4c
James Youngman <youngman@google.com>
2022-05-17 20:14:32 +0100

    Initial code for WASM app (web-based simulator).
    This actually doesn't do anything yet, so far we just have build
    infrastructure.

---
+++ wasm/src/utils.rs
@@ -0,0 +1,13 @@
+// This code is derived from
+// https://github.com/rustwasm/wasm-pack-template/blob/a3acfd295f0a10f2ddb6eebf6db10e057369df6a/src/utils.rs
+
+pub fn set_panic_hook() {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function at least once during initialization, and then
+    // we will get better error messages if our code ever panics.
+    //
+    // For more details see
+    // https://github.com/rustwasm/console_error_panic_hook#readme
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+}
