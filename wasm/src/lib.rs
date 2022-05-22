3a9a42288293dd68e5078ee4827871727926c760
James Youngman <youngman@google.com>
2022-05-22 12:47:43 +0100

    [wasm, cpu] The WASM-hosted Tx2 ticks over.
    We have no way to load a tape yet, so it doesn't do anything.

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -3,8 +3,11 @@ use std::time::Duration;
 
 mod utils;
 
-use cpu::*;
+use float_next_after::NextAfter;
+use tracing::{event, Level};
 use wasm_bindgen::prelude::*;
+
+use cpu::*;
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
@@ -20,15 +23,60 @@ pub fn start() -> Result<(), JsValue> {
     Ok(())
 }
 
+fn make_context(simulated_system_time_secs: f64, elapsed_time_secs: f64) -> Context {
+    Context {
+        simulated_time: Duration::from_secs_f64(simulated_system_time_secs),
+        real_elapsed_time: Duration::from_secs_f64(elapsed_time_secs),
+    }
+}
+
 #[wasm_bindgen]
 pub fn create_tx2(simulated_system_time_secs: f64, elapsed_time_secs: f64) -> Tx2 {
     let mem_config = MemoryConfiguration {
         with_u_memory: false,
     };
-    let context = Context {
-        simulated_time: Duration::from_secs_f64(simulated_system_time_secs),
-        real_elapsed_time: Duration::from_secs_f64(elapsed_time_secs),
-    };
+    let context = make_context(simulated_system_time_secs, elapsed_time_secs);
     let panic_on_unmasked_alarm = cpu::PanicOnUnmaskedAlarm::No;
     Tx2::new(&context, panic_on_unmasked_alarm, &mem_config)
+}
+
+#[wasm_bindgen]
+pub fn tx2_next_simulated_tick(tx2: &Tx2) -> f64 {
+    let next = tx2.next_tick();
+    // We use next_after to round f up slightly to ensure that the
+    // tick time used by the next JS call to tick() is actually a
+    // different time to the current tick.
+    let f = next.as_secs_f64().next_after(std::f64::INFINITY);
+    event!(
+        Level::INFO,
+        "tx2_next_simulated_tick: next={next:?}, f={f:?}"
+    );
+    f
+}
+
+#[wasm_bindgen]
+pub fn tx2_do_tick(tx2: &mut Tx2, simulated_time: f64, real_elapsed_time: f64) {
+    let context = make_context(simulated_time, real_elapsed_time);
+    event!(
+        Level::INFO,
+        "tx2_do_tick: simulated_time={simulated_time:?}, real_elapsed_time={real_elapsed_time:?}, context={context:?}"
+    );
+    match tx2.tick(&context) {
+        Ok(Some(_output)) => {
+            event!(Level::DEBUG, "dropping a Tx2 output event");
+        }
+        Ok(None) => (),
+        Err(e) => {
+            panic!("TX-2 alarm: {:?}", e);
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn tx2_codabo(tx2: &mut Tx2, simulated_time: f64, elapsed_time_secs: f64) {
+    event!(Level::INFO, "codabo");
+    let context = make_context(simulated_time, elapsed_time_secs);
+    tx2.codabo(&context, &ResetMode::ResetTSP);
+    tx2.set_next_execution_due(context.simulated_time, Some(context.simulated_time));
+    tx2.set_run_mode(RunMode::Running);
 }
--- wasm/www/index.js
+++ wasm/www/index.js
@@ -1,4 +1,37 @@
 import * as wasm from "tx-2-wasm";
+
+function clamped_elapsed_time(then) {
+    var now = Date.now();
+    return Math.max(now - then, 0.0);
+}
+
+function call_after(t, f) {
+    var delay_ms = t * 1000.0;
+    var id = setTimeout(f, delay_ms);
+    console.log("call_after: timeout id = " + id + ", delay_ms=" + delay_ms);
+    return id;
+}
+
+function tick_after(interval, system_time_then) {
+    call_after(interval, function() { do_tick(system_time_then) });
+}
+
+function do_tick(tick_time) {
+    var elapsed = clamped_elapsed_time(start);
+    wasm.tx2_do_tick(tx2, tick_time, elapsed);
+    var next_tick_at = wasm.tx2_next_simulated_tick(tx2);
+    var interval = next_tick_at - tick_time;
+    tick_after(interval, next_tick_at);
+}
+
+function codabo(simulated_system_time_secs, real_elapsed_time) {
+    wasm.tx2_codabo(tx2, simulated_system_time_secs, real_elapsed_time)
+}
+
 var start = Date.now();
+var sleep_multiplier = 1.0;
 
 const tx2 = wasm.create_tx2(0.0, 0.0);
+console.log("preparing for first tick");
+codabo(0.0, start)
+tick_after(1e-6, 1e-6);
