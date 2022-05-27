ab95a9c4c69056bd17e85a27a24aca0557f0004f
James Youngman <youngman@google.com>
2022-05-27 12:21:04 +0100

    Switch the web simulator to React.

---
+++ tx2-web/rust/README.md
@@ -0,0 +1,5 @@
+This is the Rust code for the web implementation.
+
+Note that you will need to rebuild manually (with e.g. `node run
+build`) if you change stuff in this folder, as the normal
+folder-watching behaviour appears not to function here.
---
+++ tx2-web/src/index.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useRef } from "react";
+import ReactDOM from "react-dom";
+import styled from 'styled-components';
+import { set_app_wasm_mod } from './model/machine'
+import Modal from 'react-modal';
+
+import { App } from './App';
+
+const wasm = import("../build/tx2_web");
+
+wasm.then(m => {
+	Modal.setAppElement('body')
+	m.init();
+	set_app_wasm_mod(m);
+	ReactDOM.render(<App />, document.getElementById("root"));
+});
