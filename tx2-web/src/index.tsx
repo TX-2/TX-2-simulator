e4d27f8bbbd5d1bc2462ee5274e3065a259a1344
James Youngman <youngman@google.com>
2022-06-14 10:58:51 +0100

    [web] display error message if the WASM code cannot be loaded.

--- tx2-web/src/index.tsx
+++ tx2-web/src/index.tsx
@@ -8,10 +8,39 @@ import { App } from './App';
 
 const wasm = import("../build/tx2_web");
 
-wasm.then(m => {
-  Modal.setAppElement('body')
-  m.init();
-  set_app_wasm_mod(m);
-  const tx2: Tx2Controller = new Tx2Controller();
-  ReactDOM.render(<App tx2Controller={tx2} />, document.getElementById("root"));
-});
+function showErrorAlert(message: string) {
+  alert("Failed to initialise the WASM code; please report this as a bug: " + message);
+}
+
+const has = <K extends string>(
+  key: K,
+  x: object,
+): x is { [key in K]: unknown } => (
+  key in x
+);
+
+function extractMessage(error: unknown): string {
+  let message = 'Unknown error';
+  if (!error) {
+    /* use the default message we already chose. */
+  } else if (typeof error == 'string') {
+    message = error;
+  } else if ((typeof error == 'object') && has('message', error) && (typeof error.message == 'string')) {
+    message = error.message;
+  }
+  return message;
+}
+
+function handleError(error: unknown) {
+  showErrorAlert(extractMessage(error))
+}
+
+wasm
+  .then(m => {
+    Modal.setAppElement('body')
+    m.init();
+    set_app_wasm_mod(m);
+    const tx2: Tx2Controller = new Tx2Controller();
+    ReactDOM.render(<App tx2Controller={tx2} />, document.getElementById("root"));
+  })
+  .catch(handleError);
