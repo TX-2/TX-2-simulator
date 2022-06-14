e2b4dfe7b2d836133458640fe9b553e704869729
James Youngman <youngman@google.com>
2022-06-14 22:05:51 +0100

    Simplify PR template by providing a script which automates checks.

---
+++ runchecks.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+base="$(basename $(pwd))"
+if ! [ "${base}" = 'TX-2-simulator' ]
+then
+    exec >&2
+    echo 'Please run this script from the top-level directory.'
+    exit 1
+fi
+
+rust_checks() {
+    cargo fmt --check &&
+	cargo test &&
+	cargo clippy
+}
+
+npm_checks() {
+    (
+	cd tx2-web &&
+	    npm run lint &&
+	    npm run build
+    )
+}
+
+if rust_checks && npm_checks
+then
+    echo "Everything is OK"
+else
+    rv=$?
+    exec >&2
+    echo
+    echo 'Please fix the problems above before committing your change.'
+    exit $rv
+fi
