ceb71084bed7ea1484b5ae34d6e268027700c132
James Youngman <youngman@google.com>
2022-10-30 11:46:35 +0000

    Superimpose the hit detector canvas on the keyboard canvas.

--- tx2-web/src/Keyboard.tsx
+++ tx2-web/src/Keyboard.tsx
@@ -1,15 +1,26 @@
 import { create_html_canvas_2d_painter, draw_keyboard } from '../build/tx2_web'
 import React, { useEffect, useRef } from 'react'
 
+type Coordinates = {
+    x: number;
+    y: number;
+};
+
 interface CanvasProps {
-    className: string | undefined,
-    draw: (context: CanvasRenderingContext2D) => void,
-    id?: string,
+  className: string | undefined,
+  draw: (context: CanvasRenderingContext2D) => void,
+  click: (
+    event: React.MouseEvent<HTMLCanvasElement, MouseEvent>,
+    canvas: HTMLCanvasElement) => void,
   width: number,
   height: number,
 }
 
-const Canvas = ({ className, draw, id, width, height, ...rest }: CanvasProps) => {
+const getCoordinates = (ev: React.MouseEvent<HTMLCanvasElement, MouseEvent>, canvas: HTMLCanvasElement): Coordinates | undefined => {
+  return {x: ev.pageX - canvas.offsetLeft, y: ev.pageY - canvas.offsetTop};
+};
+
+const Canvas = ({ className, draw, click, width, height, ...rest }: CanvasProps) => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
@@ -29,21 +40,28 @@ const Canvas = ({ className, draw, id, width, height, ...rest }: CanvasProps) =>
             };
         }
     }, [draw])
-    console.log("rendering the canvas...");
+
+  const doClick = (ev: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    click(ev, canvas)
+  }
+
+  console.log("rendering the canvas...");
   return <canvas
     ref={canvasRef}
     className={className}
-    id={id}
     width={width}
     height={height}
+    onClick={doClick}
     {...rest} />
 }
-
 
 interface KeyboardProps {
   className?: string | undefined,
   hdClass?: string | undefined,
-  id?: string,
 }
 
 const Keyboard = (props: KeyboardProps) => {
@@ -60,6 +78,25 @@ const Keyboard = (props: KeyboardProps) => {
     const draw_hitdetect = (ctx: CanvasRenderingContext2D) => {
         draw(ctx, true)
     }
+    const click_hitdetect = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>, canvas: HTMLCanvasElement) => {
+      // Not yet implemented.
+      console.log("in Canvas click callback for hit detector canvas, it's not implemented.");
+      console.log({event});
+      const context = canvas.getContext('2d');
+      if (!context) {
+        return;
+      }
+      const clickPos = getCoordinates(event, canvas)
+      if (!clickPos) {
+        return;
+      }
+      const data = context.getImageData(clickPos.x, clickPos.y, 1, 1).data;
+      console.log("RGB(A) value (on hit detector canvas) at click position is", data)
+      //var rgb = [ data[0], data[1], data[2] ];
+    }
+    const click_keyboard = (_event: React.MouseEvent, _canvas: HTMLCanvasElement) => {
+      // We don't need to do anything, the work is done in click_hitdetect.
+    }
   const w = 800;
   const h = 14.5 / 23.8 * w;
   // We draw two canvases; the first is visible and shows the actual
@@ -68,15 +105,15 @@ const Keyboard = (props: KeyboardProps) => {
   return (<div>
     <Canvas
       className={props.className}
-      id={props.id}
       draw={draw_vis}
+      click={click_keyboard}
       width={w}
       height={h}
     />
     <Canvas
       className={props.hdClass}
-      id={props.id}
       draw={draw_hitdetect}
+      click={click_hitdetect}
       width={w}
       height={h}
     />
