TX-2/TX-2-simulator

commit 14799ef899bca697ef42d8be9611093b69cc8d5e

[base] Clippy: enable and fix match_same_arms.

base/src/charset.rs
@@ -425,6 +425,10 @@ pub fn lincoln_char_to_described_char(
     lincoln_writer_state_update(lin_ch, state);
     let advance: bool = lin_ch != 0o12 && lin_ch != 0o13;
     let by_case = |lower, upper: char| -> Option<char> { Some(bycase(lower, upper, *state)) };
+
+    // It's more important for the cases to be in numerical order than
+    // it is to avoid identical bodies.
+    #[allow(clippy::match_same_arms)]
     let base_char: Option<char> = match u8::from(lin_ch) {
         0o00 => by_case('0', '☛'), // \U261B, black hand pointing right
         0o01 => by_case('1', 'Σ'), // \U03A3, Greek capital letter Sigma
@@ -547,8 +551,11 @@ pub fn lincoln_char_to_described_char(
         // Non-carriage-advancing characters don't strictly match the
         // key label, because we represent them as combining
         // characters and so there's a space in the key label too.
-        let label_matches_unicode = advance
-            && match display {
+        let label_matches_unicode = if advance {
+            true
+        } else {
+            #[allow(clippy::match_same_arms)]
+            match display {
                 None => false,
                 Some(' ') => {
                     // Here the mapping is to ' ' but in the keyboard
@@ -565,7 +572,8 @@ pub fn lincoln_char_to_described_char(
                     false
                 }
                 Some(_) => true,
-            };
+            }
+        };
         Some(DescribedChar {
             base_char: LincolnChar::UnicodeBaseChar(base),
             unicode_representation: display,

base/src/instruction.rs
@@ -450,6 +450,7 @@ impl TryFrom<u8> for Opcode {
     type Error = DisassemblyFailure;
     fn try_from(opcode: u8) -> Result<Opcode, DisassemblyFailure> {
         use Opcode::*;
+        #[allow(clippy::match_same_arms)] // numerical order
         match opcode {
             // TODO: change these opcode values to octal.
             0 | 1 => Err(DisassemblyFailure::InvalidOpcode(opcode)),

base/src/instruction/format.rs
@@ -155,20 +155,23 @@ fn write_opcode(
         } else {
             "IOS"
         }),
-        Opcode::Jmp => f.write_str(match cfg {
-            0o00 => "JMP",
-            0o01 => "BRC",
-            0o02 => "JPS",
-            0o03 => "BRS",
-            0o14 => "JPQ",
-            0o15 => "BPQ",
-            0o16 => "JES",
-            0o20 => "JPD",
-            0o21 => "JMP",
-            0o22 => "JDS",
-            0o23 => "BDS",
-            _ => "JMP",
-        }),
+        Opcode::Jmp => f.write_str(
+            #[allow(clippy::match_same_arms)] // numerical order
+            match cfg {
+                0o00 => "JMP",
+                0o01 => "BRC",
+                0o02 => "JPS",
+                0o03 => "BRS",
+                0o14 => "JPQ",
+                0o15 => "BPQ",
+                0o16 => "JES",
+                0o20 => "JPD",
+                0o21 => "JMP",
+                0o22 => "JDS",
+                0o23 => "BDS",
+                _ => "JMP",
+            },
+        ),
         Opcode::Skx => f.write_str(match cfg {
             0o00 => "REX",
             0o02 => "INX",
@@ -181,32 +184,35 @@ fn write_opcode(
             0o30 => "RFD",
             _ => "SKX",
         }),
-        Opcode::Skm => f.write_str(match cfg {
-            0o00 => "SKM",
-            0o01 => "MKC",
-            0o02 => "MKZ",
-            0o03 => "MKN",
-            0o10 => "SKU",
-            0o11 => "SUC",
-            0o12 => "SUZ",
-            0o13 => "SUN",
-            0o20 => "SKZ",
-            0o21 => "SZC",
-            0o22 => "SZZ",
-            0o23 => "SZN",
-            0o30 => "SKN",
-            0o31 => "SNC",
-            0o32 => "SNZ",
-            0o33 => "SNN",
-            0o04 => "CYR",
-            0o05 => "MCR",
-            0o06 => "MZR",
-            0o07 => "MNR",
-            0o34 => "SNR",
-            0o24 => "SZR",
-            0o14 => "SUR",
-            _ => "SKM",
-        }),
+        Opcode::Skm => f.write_str(
+            #[allow(clippy::match_same_arms)] // numerical order
+            match cfg {
+                0o00 => "SKM",
+                0o01 => "MKC",
+                0o02 => "MKZ",
+                0o03 => "MKN",
+                0o10 => "SKU",
+                0o11 => "SUC",
+                0o12 => "SUZ",
+                0o13 => "SUN",
+                0o20 => "SKZ",
+                0o21 => "SZC",
+                0o22 => "SZZ",
+                0o23 => "SZN",
+                0o30 => "SKN",
+                0o31 => "SNC",
+                0o32 => "SNZ",
+                0o33 => "SNN",
+                0o04 => "CYR",
+                0o05 => "MCR",
+                0o06 => "MZR",
+                0o07 => "MNR",
+                0o34 => "SNR",
+                0o24 => "SZR",
+                0o14 => "SUR",
+                _ => "SKM",
+            },
+        ),
         _ => write!(f, "{op}"),
     }
 }

base/src/lib.rs
@@ -19,6 +19,7 @@
 #![warn(clippy::doc_markdown)]
 #![warn(clippy::semicolon_if_nothing_returned)]
 #![warn(clippy::return_self_not_must_use)]
+#![warn(clippy::match_same_arms)]
 #![allow(clippy::cast_possible_wrap)]
 #![allow(clippy::cast_sign_loss)]
 #![allow(clippy::needless_continue)] // fix these later

commit 4f691bc891fb0d9c04db08eb3616fcfbe4c09f30

Fix bug introduced in 14799ef899bca697ef42d8be9611093b69cc8d5e.

base/src/charset.rs
@@ -551,8 +551,8 @@ pub fn lincoln_char_to_described_char(
         // Non-carriage-advancing characters don't strictly match the
         // key label, because we represent them as combining
         // characters and so there's a space in the key label too.
-        let label_matches_unicode = if advance {
-            true
+        let label_matches_unicode = if !advance {
+            false
         } else {
             #[allow(clippy::match_same_arms)]
             match display {